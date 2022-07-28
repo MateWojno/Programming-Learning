0073bcfcc50652b9bae4aedd93c1fba0045e5b96
MateWojno <mateusz.k.wojno@gmail.com>
2022-07-29 00:51:50 +0200

    .

--- Program.cs
+++ Program.cs
@@ -3,11 +3,28 @@
 
 namespace HelloWorld
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+
+class Program //class
+
+{
+static void Main(string[] args) //method
+{
+    Console.WriteLine("Hello World! \n this time we will play a simple game"); //print in Python
 }
+
+void BallGame()
+{
+    
+}
+    public float ballSpeed;
+    public string ballName = "the Black One";
+    public bool ballColision = false;
+
+    public float enemyLocation; 
+}
+   
+
+
+}
+
+
