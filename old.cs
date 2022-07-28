38ec318f0e8b05e39aad327f9c3707798a94943e
MateWojno <mateusz.k.wojno@gmail.com>
2022-07-29 01:41:49 +0200

    .

---
+++ old.cs
@@ -0,0 +1,9 @@
+    /*string myName = "MateWojno";
+    
+    Console.WriteLine(myName + "Set double x: ");
+    double x = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine(myName + "Set double y: ");
+    double y = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine(myName + "Set double z: ");
+    double z = Convert.ToDouble(Console.ReadLine());
+    */
